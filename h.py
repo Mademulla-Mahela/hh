ef62076942e0841cbaafd8a9bd41a5d263f6ea5f
Mademulla Mahela <kushan.20231308@iit.ac.lk>
2024-01-01 10:00:00 +0530

    Commit made on 2024-01-01T10:00:00+05:30

--- h.py
+++ h.py
@@ -1,9 +1,3 @@
-#Task 7: Personal Information Script:
-    
-name = input("enter your name: ")
-print(f" hello, {name}! it's nice to meet you")
-age = int(input("enter your age"))
-print(f"your, {age} . wow! your doing great")
-color = input ("enter your favorite color: ")
-print(f"nice, {color} is a great choice!")
-jjj
+{
+  "date": "2024-01-01T10:00:00+05:30"
+}
