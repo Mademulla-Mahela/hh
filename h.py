a8a3aa987826309ba5ca25483bec886ef3a47442
Mademulla Mahela <kushan.20231308@iit.ac.lk>
2024-11-03 12:03:21 +0530

    Add files via upload

---
+++ h.py
@@ -0,0 +1,8 @@
+#Task 7: Personal Information Script:
+    
+name = input("enter your name: ")
+print(f" hello, {name}! it's nice to meet you")
+age = int(input("enter your age"))
+print(f"your, {age} . wow! your doing great")
+color = input ("enter your favorite color: ")
+print(f"nice, {color} is a great choice!")
